4a09b8283a5b4bb8eba349cf67cb439009fc696c
Darryl Barnes <dlbarnes@dbglobalinvestments.com>
2026-06-28 10:23:41 -0500

    Fix prisma default export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,6 +8,8 @@ export const prisma =
     log: ["query"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
